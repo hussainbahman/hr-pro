25f34d2e8a1cee7981b3d1c8788167d23d54d6dc
hussainbahman <Hbahmanq8@gmail.com>
2023-01-11 20:42:13 +0300

    save the folder

--- hr_pro.py
+++ hr_pro.py
@@ -1,11 +1,46 @@
-class Employee:
+class Employee():
+    def __init__(self, name, age, salary, employment_years, ):
+       self.name = name
+       self.age = age
+       self.salary = salary
+       self.employment_years = employment_years
+    def get_annual_salary(self):
+        return self.salary*12
+
+
+    def __str__(self):
+        return f"my name is {self.name} i'm {self.age} years old my salary {self.salary} working {self.employment_years} years "
+        
+
+    
+
+
+
+       
+
+
    #Employee class here
 
 class Manager(Employee):
+    def __init__(self, name, age, salary, employment_years, bonus_percentage ):
+        super().__init__(name, age, salary,employment_years )
+        self.bonus_percentage = bonus_percentage
+
+
+        def info(self):
+            print("the bonus is {bonus_percentage}")
+
+
+print(Employee("hussain",18,5000,5))
+
+
+
+
     #Manager class here
         
-def main():
+def main(self):
 	# main code here
 
-if __name__ == '__main__':
-	main()
+ if __name__ == '__main__':
+	 main()
+    
